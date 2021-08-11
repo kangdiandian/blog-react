9672f3f79ff21e548bcc4d6360cac08922a0ac2a
康路路 <lulu.kang@weimob.com>
2021-08-11 17:33:22 +0800

    up

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit a nd saqsqe to o ad.
+          Edit a nd saqsssqe to o ad.
         </p>
         <a
           className="App-link"
